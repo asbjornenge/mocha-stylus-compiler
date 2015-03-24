c423b3a0ad3ba65badce147ad22ee1c23dbfe31f
Asbjørn Enge <asbjorn@hanafjedle.net>
2015-03-24 10:12:38 +0100

    Correctly escaping css string

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var fs     = require('fs')
 var stylus = require('stylus')
+var esc    = require('js-string-escape')
 var exts   = ['.styl']
 var orig   = require.extensions['.js']
 
@@ -7,7 +8,7 @@ var loader = function(ext, module, filename) {
     if (filename.indexOf('node_modules') >= 0) return orig(module, filename)
     var content = fs.readFileSync(filename, 'utf-8')
     var css = stylus(content).render()
-    return module._compile('module.exports="'+css.replace(/\n/g,'')+'"', filename)
+    return module._compile('module.exports="'+esc(css)+'"', filename)
 }
 
 exts.forEach(function(ext) {
--- test/spec.js
+++ test/spec.js
@@ -4,3 +4,7 @@ var app_style = require('./app')
 it('enables requiring of stylus files', function() {
     assert(app_style.indexOf('background-color:') >= 0)
 })
+
+it('correctly escapes', function() {
+    assert(app_style.indexOf('url(\"' > 0)) 
+})
